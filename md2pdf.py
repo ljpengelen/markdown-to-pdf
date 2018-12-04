14ed50fd4f20d338f584127ad00f36594f075465
Luc Engelen <luc@kabisa.nl>
2018-12-04 22:30:08 +0100

    Watch files and generate HTML and PDF documents

--- md2pdf.py
+++ md2pdf.py
@@ -1,24 +1,25 @@
-import codecs
 import os
+import time
 import click
 import markdown
+from markdown_include.include import MarkdownInclude
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
 from weasyprint import HTML
-from werkzeug.serving import run_simple
-from werkzeug.wrappers import Response
-from werkzeug.wsgi import responder
 
 EXISTING_FILE = click.Path(exists=True, dir_okay=False, resolve_path=True)
 
 
 def _html(markdown_file_name, css_file_name):
-    with codecs.open(markdown_file_name, mode="r", encoding="utf-8") as markdown_file:
-        with codecs.open(css_file_name, mode="r", encoding="utf-8") as css_file:
+    with open(markdown_file_name, mode="r", encoding="utf-8") as markdown_file:
+        with open(css_file_name, mode="r", encoding="utf-8") as css_file:
             markdown_input = markdown_file.read()
             css_input = css_file.read()
 
+            markdown_path = os.path.dirname(markdown_file_name)
+            markdown_include = MarkdownInclude(configs={"base_path": markdown_path})
             html = markdown.markdown(
-                markdown_input,
-                extensions=["extra", "markdown_include.include", "meta", "tables"],
+                markdown_input, extensions=["extra", markdown_include, "meta", "tables"]
             )
 
             return f"""
@@ -31,12 +32,29 @@ def _html(markdown_file_name, css_file_name):
             """
 
 
-def _app(markdown_file_name, css_file_name):
-    @responder
-    def respond(_environ, _start_response):
-        return Response(_html(markdown_file_name, css_file_name), mimetype="text/html")
+def _convert(markdown_file_name, css_file_name):
+    file_name = os.path.splitext(markdown_file_name)[0]
+    html_string = _html(markdown_file_name, css_file_name)
 
-    return respond
+    with open(
+        file_name + ".html", "w", encoding="utf-8", errors="xmlcharrefreplace"
+    ) as output_file:
+        output_file.write(html_string)
+
+    markdown_path = os.path.dirname(markdown_file_name)
+    html = HTML(string=html_string, base_url=markdown_path)
+    html.write_pdf(file_name + ".pdf")
+
+
+class EventHandler(FileSystemEventHandler):
+    def __init__(self, markdown_file_name, css_file_name):
+        self.markdown_file_name = markdown_file_name
+        self.css_file_name = css_file_name
+
+    def on_modified(self, event):
+        if event.src_path in [self.markdown_file_name, self.css_file_name]:
+            click.echo(f"{event.src_path} has changed")
+            _convert(self.markdown_file_name, self.css_file_name)
 
 
 @click.group()
@@ -47,19 +65,39 @@ def cli():
 @cli.command()
 @click.argument("md", type=EXISTING_FILE)
 @click.argument("css", type=EXISTING_FILE)
-def serve(md, css):
-    """Converts Markdown file MD and stylesheet CSS, and serves result."""
-    run_simple("localhost", 8080, _app(md, css))
+def watch(md, css):
+    (
+        "Watches Markdown file MD and stylesheet CSS for changes, "
+        "and converts them to HTML and PDF documents when they are modified."
+    )
+    click.echo(f"Watching {md} and {css} for changes. Press CTRL+C to quit.")
+
+    _convert(md, css)
+
+    markdown_path = os.path.dirname(md)
+    css_path = os.path.dirname(css)
+
+    event_handler = EventHandler(md, css)
+    observer = Observer()
+    observer.schedule(event_handler, markdown_path)
+    if markdown_path != css_path:
+        observer.schedule(event_handler, css_path)
+    observer.start()
+
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
 
 
 @cli.command()
 @click.argument("md", type=EXISTING_FILE)
 @click.argument("css", type=EXISTING_FILE)
 def convert(md, css):
-    """Converts Markdown file MD and stylesheet CSS to PDF."""
-    html = HTML(string=_html(md, css), base_url=".")
-    file_name = os.path.splitext(md)[0] + ".pdf"
-    html.write_pdf(file_name)
+    """Converts Markdown file MD and stylesheet CSS to HTML and PDF documents."""
+    _convert(md, css)
 
 
 if __name__ == "__main__":
